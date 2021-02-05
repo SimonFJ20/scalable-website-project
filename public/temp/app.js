16910afa77ae8e92d96abcbecd05842171fadeb6
Simon From <simonfromjakobsen@gmail.com>
2021-02-05 16:57:08 +0100

    Delete public/temp directory

--- public/temp/app.js
+++
@@ -1,7 +0,0 @@
-import { html, importCSS } from './framework.ts';
-import Loginform from './loginform';
-var App = function () {
-    importCSS('./style.css');
-    return html("\n    " + Loginform() + "\n    ");
-};
-export default App;
--- public/temp/loginform.js
+++
@@ -1,5 +0,0 @@
-import { html } from "./framework";
-var Loginform = function () {
-    return html("\n    <div id=\"loginform\">\n        <input id=\"username\" type=\"text\" placeholder=\"Username\">\n        <br>\n        <input id=\"password\" type=\"text\" placeholder=\"Password\">\n        <br>\n        <input id=\"login\" type=\"button\" value=\"Login\">\n    </div>\n    ");
-};
-export default Loginform;
