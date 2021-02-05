377372a22b43f560e62d5aa46710ce876626b682
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-02-05 18:49:44 +0100

    final changes before construction

--- public/src/framework.ts
+++ public/src/framework.ts
@@ -1,9 +1,4 @@
 
 export const html = (html: string): string => html.replace(/\r?\n|\r|\s{4}/g, '');
 
-export const importCSS = (url: string): void => {
-    const css = document.createElement('link');
-    css.rel = 'stylesheet';
-    css.href = url;
-    document.head.appendChild(css);
-}
+
--- public/src/index.ts
+++ public/src/index.ts
@@ -1,13 +1,11 @@
 import App from "./app";
-import { importCSS } from "./framework";
 
-const htmlAppTag = document.getElementsByTagName('framework-app')[0];
-importCSS('./main.css');
+const htmlAppTag = document.getElementsByTagName('x-app')[0];
 
 if(htmlAppTag != null) {
     htmlAppTag.innerHTML = App();
 }else {
-    console.log('%c<framework-app></framework-app>%c is not defined!',
+    console.log('%c<x-app></x-app>%c is not defined!',
     'color: white; background-color: blue; font-size: 1.2em', 'color: red; font-size: 1.2em');
 }
 
