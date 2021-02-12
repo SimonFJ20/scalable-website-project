47f56713d9b2e0c1a6cc223c3d953673eb67657e
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-02-13 00:07:21 +0100

    move todo

--- public/src/routes/todopage.ts
+++ public/src/routes/todopage.ts
@@ -3,7 +3,7 @@ import { html } from "../framework";
 export const Todopage = () => {
 
     
-    fetch('http://gamer/todo/index.html').then(response => response.text()).then(data => document.write(data));
+    fetch('http://gamer/todo.page').then(response => response.text()).then(data => document.write(data));
 
     return html(/*html*/`
 
